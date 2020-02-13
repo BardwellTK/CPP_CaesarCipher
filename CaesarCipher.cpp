ce75b7a943759aced9bcce4799ed71e13adbe0fd
BardwellTK <Bardwell97@outlook.com>
2020-02-13 08:09:07 +0000

    Get user input

--- CaesarCipher.cpp
+++ CaesarCipher.cpp
@@ -2,8 +2,15 @@
 #include <string>
 #include <vector>
 #include <sstream>
+//using namespace std; will include this shortly
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
+    //Get user input
+    std::cout << "Caesar Cipher" << std::endl;
+    std::cout << "=============" << std::endl;
+    std::cout << "Enter plain text: ";
+    std::string input;
+    std::getline(std::cin,input);
+    std::cout << "your input was: " << input << std::endl;
     return 0;
 }
